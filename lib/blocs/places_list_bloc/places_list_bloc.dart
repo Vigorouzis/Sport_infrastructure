7b01132b71850368e1a048729c1e79a41720d3bd
Nikita <Nkanunov@gmail.com>
2021-01-13 17:42:40 +0200

    Created places_list_bloc bloc

--- lib/blocs/places_list_bloc/places_list_bloc.dart
+++ lib/blocs/places_list_bloc/places_list_bloc.dart
@@ -1,25 +1,36 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sport_infrastructure/blocs/places_list_bloc/places_list_event.dart';
 import 'package:sport_infrastructure/blocs/places_list_bloc/places_list_state.dart';
+import 'package:sport_infrastructure/models/place.dart';
 
-class PlacesListBloc extends Bloc<PlacesListEvent, PlacesListState>{
+class PlacesListBloc extends Bloc<PlacesListEvent, PlacesListState> {
   //TODO: Created http controller class
-  PlacesListBloc(PlacesListState initialState) : super(InitialPlacesListState());
-
-
+  PlacesListBloc() : super(InitialPlacesListState());
 
   @override
   Stream<PlacesListState> mapEventToState(PlacesListEvent event) async* {
-   if(event is PlacesListFetched) {
-     try {
-       yield PlacesListLoading();
-       //TODO: Get data from server
-       yield PlacesListLoaded();
-     }
-     catch (_){
-       yield PlacesListFailure();
-     }
-   }
+    if (event is PlacesListFetched) {
+      yield PlacesListLoading();
+      try {
+        //TODO: Get data from server
+        var places = [
+          Place(
+              id: '1',
+              name: 'Nikita',
+              description: 'Kanunov',
+              isFreeVisit: true,
+              abonements: null,
+              basedOrganizations: null,
+              holderOrganization: null,
+              location: null,
+              categoriesIDs: null,
+              tagsUIDs: null)
+        ];
+        Future.delayed(const Duration(milliseconds: 500));
+        yield PlacesListLoaded(places: places);
+      } catch (_) {
+        yield PlacesListFailure();
+      }
+    }
   }
-
 }
