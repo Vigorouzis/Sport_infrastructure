14a47795a8c54442bd3c4cceb37457e27ac4d0af
Nikita <Nkanunov@gmail.com>
2021-01-21 16:47:57 +0200

    Created place_provider for get place data from server

---
+++ lib/resources/place_provider.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:sport_infrastructure/models/place.dart';
+
+class PlaceProvider{
+  static const String BASE_URL = '';
+
+  Future<List<Place>> getPlace() async{
+
+      Response response = await Dio().get(BASE_URL);
+
+      if(response.statusCode >= 200 && response.statusCode <=299){
+        final List<dynamic> placeJson = json.decode(response.data);
+        return placeJson.map((json) => Place.fromJson(json)).toList();
+      }else{
+        throw Exception('Error fetching places');
+      }
+
+  }
+
+}
