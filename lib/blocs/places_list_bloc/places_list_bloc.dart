84281384ccc49d0bf1c4cf32abd043eee71be223
Nikita <Nkanunov@gmail.com>
2021-01-21 17:26:34 +0200

    Added placeRepository

--- lib/blocs/places_list_bloc/places_list_bloc.dart
+++ lib/blocs/places_list_bloc/places_list_bloc.dart
@@ -2,10 +2,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sport_infrastructure/blocs/places_list_bloc/places_list_event.dart';
 import 'package:sport_infrastructure/blocs/places_list_bloc/places_list_state.dart';
 import 'package:sport_infrastructure/models/place.dart';
+import 'package:sport_infrastructure/resources/place_repository.dart';
 
 class PlacesListBloc extends Bloc<PlacesListEvent, PlacesListState> {
-  //TODO: Created http controller class
-  PlacesListBloc() : super(InitialPlacesListState());
+  final PlaceRepository placeRepository;
+
+  PlacesListBloc(this.placeRepository) : super(InitialPlacesListState());
 
   @override
   Stream<PlacesListState> mapEventToState(PlacesListEvent event) async* {
